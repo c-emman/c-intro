15bc537a770ee59d416dc0bb4120a38bf1186cab
christianemmanuel <chris.emmanuel@hotmail.com>
2026-03-21 20:26:30 +0000

    Added support for multiplication and division

--- calculator.c
+++ calculator.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 
-int computeOp(char p, char *cOp, int *rSum, int *cNum);
+int computeOp(char p, char *cOp, int *rSum, int *cNum, char *cumOp, int *cSum);
+// 3 + 5 * 2 - 1
+// cOp = +; rSum = 3;
 
-int computeOp(char p, char *cOp, int *rSum, int *cNum) {
-	if ( p == '+' || p == '-') {
+int flushCumSum(int *cNum, char *cumOp, int *cSum);
+
+int flushCumSum(int *cNum, char *cumOp, int *cSum) {
+	if (*cumOp != '\0') {
+		*cSum = *cumOp == '*' ? *cSum * *cNum : *cSum / *cNum;
+		*cNum = *cSum;
+		*cumOp = '\0';
+		*cSum = 0;
+	}
+	return 0;
+}
+
+
+
+int computeOp(char p, char *cOp, int *rSum, int *cNum, char *cumOp, int *cSum) {
+	if ( p == '+' || p == '-' ) {
 		if (*cOp == '\0') {
 			*cOp = p == '+' ? '+' : '-';
-			*rSum = *cNum;
+			flushCumSum(cNum, cumOp, cSum);
+			*rSum += *cNum;
 			*cNum = 0;
 		}
 		else if (*cOp == '+' || *cOp == '-') {
+			flushCumSum(cNum, cumOp, cSum);
 			*rSum = *cOp == '+' ? *rSum + *cNum : *rSum - *cNum;
 			*cNum = 0;
 			*cOp = p;
 		}
 	}
-        else {
-                *cNum = *cNum * 10 + (p - '0');
-        }
-        return 0;
+	else if ( p == '/' || p == '*' ) {
+		if (*cumOp == '\0') {
+			*cumOp = p == '*' ? '*' : '/';
+			*cSum = *cNum;
+			*cNum = 0;
+		}
+		else if (*cumOp == '*' || *cumOp == '/') {
+			*cSum = *cumOp == '*' ? *cSum * *cNum : *cSum / *cNum;
+			*cumOp = p;
+			*cNum = 0;
+		}
+	}
+	else {
+		*cNum = *cNum * 10 + (p - '0');
+	}
+	return 0;
 }
 
 int main(void) 
@@ -28,20 +58,23 @@ int main(void)
 	int currentNum = 0;
 	int rollingSum = 0;
 	char currentOp = '\0';
+	char cumutativeOp = '\0';
+	int cumutativeSum = 0;
 
 	printf("Type in a sum into the calculator \n");
 	scanf("%255s", inputNum );
 
 	for (char *p = inputNum; *p != '\0'; p++) {
-		computeOp(*p, &currentOp, &rollingSum, &currentNum);
+		computeOp(*p, &currentOp, &rollingSum, &currentNum, &cumutativeOp, &cumutativeSum);
 	}
 	
-	if ( currentOp == '+' ) {
-		rollingSum += currentNum;
+	flushCumSum(&currentNum, &cumutativeOp, &cumutativeSum);
+	if (currentOp != '\0'){
+		rollingSum = currentOp == '+' ? rollingSum + currentNum : rollingSum - currentNum;
+	} else {
+		rollingSum = currentNum;
 	}
-	else if (currentOp == '-') {
-		rollingSum -= currentNum;
-	}
+	
 	printf("Answer:::: ");
 	printf("%d", rollingSum);
 	printf("\n");
